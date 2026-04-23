87090f803944cd0e68ec80c0e5b331c8efe339f4
Alexander <caicedoficial@gmail.com>
2026-04-23 14:19:00 -0500

    feat(payments): anadir FKs de caja menor y legalizacion a invoice_payments

---
+++ config/Migrations/20260422120000_AddRecordFksToInvoicePayments.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\BaseMigration;
+
+class AddRecordFksToInvoicePayments extends BaseMigration
+{
+    public function up(): void
+    {
+        $this->table('invoice_payments')
+            ->addColumn('petty_cash_record_id', 'integer', [
+                'null' => true,
+                'default' => null,
+                'after' => 'payment_scheduling_id',
+            ])
+            ->addColumn('legalization_record_id', 'integer', [
+                'null' => true,
+                'default' => null,
+                'after' => 'petty_cash_record_id',
+            ])
+            ->addForeignKey('petty_cash_record_id', 'petty_cash_records', 'id', [
+                'delete' => 'SET_NULL',
+                'update' => 'NO_ACTION',
+            ])
+            ->addForeignKey('legalization_record_id', 'legalization_records', 'id', [
+                'delete' => 'SET_NULL',
+                'update' => 'NO_ACTION',
+            ])
+            ->update();
+    }
+
+    public function down(): void
+    {
+        $this->table('invoice_payments')
+            ->dropForeignKey('petty_cash_record_id')
+            ->dropForeignKey('legalization_record_id')
+            ->removeColumn('petty_cash_record_id')
+            ->removeColumn('legalization_record_id')
+            ->update();
+    }
+}
